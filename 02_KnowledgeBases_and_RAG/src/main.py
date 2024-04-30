bad144935fe290350c3b270f20a3b7bac6ba2b9e
renya <rsmilek5492@gmail.com>
2024-04-30 23:16:15 +0900

    update

--- 02_KnowledgeBases_and_RAG/src/main.py
+++ 02_KnowledgeBases_and_RAG/src/main.py
@@ -3,7 +3,7 @@ import logging
 
 from llm import LLM
 from retriever import Retriever
-from prompt_template import PromptTemplate
+from prompt_template import PromptConfig
 from botocore.exceptions import ClientError
 
 
@@ -13,23 +13,21 @@ logging.basicConfig(level=logging.INFO)
 def main():
     kb_id = "JWZYVDJFCU"
     region = "us-west-2"
-    path = "./config/claude_cofig.yaml"
-    prompt_template = PromptTemplate(path)
+    config_path = "./config/claude_cofig.yaml"
+    template_path = "./config/prompt_template.yaml"
 
-
+    prompt_conf = PromptConfig(config_path, template_path)
     retriever = Retriever(kb_id, region)
     llm = LLM(region)
 
-    retrieval_results = retriever.retrieve(prompt_template.query)
+    retrieval_results = retriever.retrieve(prompt_conf.query)
     contexts = retriever.get_contexts(retrieval_results)
-    prompt = prompt_template.format_prompt({"contexts": contexts, "query": prompt_template.query})
-    # print(prompt)
-    prompt_template.format_message(prompt)
-
+    prompt_conf.format_prompt({"contexts": contexts, "query": prompt_conf.query})
+    prompt_conf.format_message({"prompt": prompt_conf.prompt})
 
     try:
-        body = json.dumps(prompt_template.config)
-        response_body = llm.generate_message(body, prompt_template.model_id)
+        body = json.dumps(prompt_conf.config)
+        response_body = llm.generate_message(body, prompt_conf.model_id)
         print(response_body["content"][0]["text"])
     except ClientError as err:
         message=err.response["Error"]["Message"]
--- 02_KnowledgeBases_and_RAG/src/prompt_config.py
+++ 02_KnowledgeBases_and_RAG/src/prompt_config.py
@@ -1,30 +1,24 @@
+import copy
 import yaml
 from typing import Any
 
 
-class PromptTemplate():
-    def __init__(self, config_path):
+class PromptConfig():
+    def __init__(self, config_path, template_path):
         self.config = self._load_config(config_path)
-        self.template = self.config.pop("template")
+        self.config_org = copy.deepcopy(self.config)
+        self.template = self._load_config(template_path)["template"]
         self.query = self.config.pop("query")
         self.model_id = self.config.pop("modelId")
+        self.prompt = ""
 
     def _load_config(self, config_path):
         with open(config_path, "r") as file:
             return yaml.safe_load(file)
 
     def format_prompt(self, args):
-        return self.template.format(**args)
+        self.prompt = self.template.format(**args)
 
-    def format_message(self, prompt):
-        # print(self.config["messages"][0]["content"][0]["text"])
-        self.config["messages"][0]["content"][0]["text"] = prompt
-        print(self.config["messages"][0]["content"][0]["text"])
-
-
-if __name__ == "__main__":
-    path = "/home/renya/Develop/aws/amazon-bedrock-workshop/02_KnowledgeBases_and_RAG/src/config/claude_cofig.yaml"
-    prompt = PromptTemplate()
-    prompt.load_config(path)
-    prompt.set_template()
-    print(prompt)
+    def format_message(self, args):
+        message = self.config["messages"][0]["content"][0]["text"]
+        self.config["messages"][0]["content"][0]["text"] = message.format(**args)
